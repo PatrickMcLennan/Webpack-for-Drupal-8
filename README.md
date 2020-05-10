3a27e1c63f6ccaa6244118aae870cf3c0bbd22cf
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-05-10 15:09:01 -0400

    Added webpack config, TypeScript support, working on Jest integration

---
+++ README.md
@@ -0,0 +1,61 @@
+# Webpack for Drupal 8
+
+**Note -- This is still Project & readme are a work in progress**
+
+#### What is this?
+
+Webpack for Drupal 8 is my attempt to create the most hands off, reactive Webpack configuration for F/E developers to use when working on a Drupal 8 project. The goal was to have something that needed no tampering, no adding, no changes -- **something that just works on each project as it grows.** Webpack for Drupal 8 does that - recursively searching through your themes and modules within `docroot`, it will compile all CSS + JS within, only applying to your source code as it goes.
+
+TL;DR -- It's an opinionated Webpack configuration allowing you to use modern tools within Drupal 8 without needing to alter.
+
+#### Why?
+
+Despite being incredibly powerful, Webpack is still an incredibly convulated tool to implement. Making things more difficult, between modules and themes Drupal has an intricate file structure that we need to gracefully maneuver within. This makes using modern tools -- such as React, TypeScript, SASS, etc. -- difficult to compile / transpile in a truly programatic way. Most developers resort to manually adding paths and files to their build tools.
+
+#### How does it work?
+
+Webpack for Drupal 8 relies on a naming convention for your modules and themes. We go into the basic usage below, but the rules you should know are:
+
+- All developer source code for modules or themes should live within a `custom` directory, and each module and theme should have a `src` directory.
+  - `docroot/themes/custom/your_theme`
+  - `docroot/modules/custom/your_module`
+- Within your theme or module folder, **you must have an entry point JS file for all source code.** All CSS or JS should imported _or_ made available recursively through this entry point, or it will not get compiled. This entry points name will always be the name of your compiled code, so it's recommended this be named the same as the module.
+  - ex - A `my_module.js` entry point will produce `my_module.css` and `my_module.js` compiled code. We'll go into this more below.
+
+and that's it! Follow those 2 rules and you're ready.
+
+#### How can I use this?
+
+There are currently 3 methods you can run this configuration:
+
+- `npm run watch` - this starts the configuration in watch mode. Upon any saved change to a file that an entry file knows about, all code will recompile. This method gives you source-mapping.
+- `npm run build:dev` - this gives you a Development build. Produces the exact same code as `npm run watch`
+- `npm run build:prod` - this gives you a Production build. Code is minified and source-mapping is stripped out.
+
+#### What does this support?
+
+This currently supports:
+
+- SCSS
+- ES6
+- TypeScript
+- React + Styled Components
+
+So long as these files are imported either directly or recursively into your entry file, you will recieve either ES5 output and auto-prefixed CSS.
+
+Still a work in progress:
+
+- Jest Testing Framework
+
+#### I'm sold. Let's get started.
+
+Place the configuration files in this repo in the root of your project, and follow the naming convention above.
+
+- Theme example:
+  - ![A Theme example](https://imgur.com/nBtozbJ)
+  - We have our `custom/new_theme` directory, and inside, a `src/new_theme.js`, with `new_theme.js` being our entry point for `new_theme`.
+- Module example:
+  - ![A Module example](https://imgur.com/rStf8HY)
+  - We have our `custom/new_module` directory, and inside, a `src/new_module.js`, with `new_module.js` being our entry for for `new_module`.
+
+So long as you follow this convention, you can do this for as many modules or themes as you'd like. No manual configuration within Webpack is necessary.
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const globby = require("globby");
+const { resolveTsAliases } = require("resolve-ts-aliases");
 
 const logError = (error) => {
   console.clear();
@@ -12,12 +13,18 @@ const logError = (error) => {
   return process.exit(1);
 };
 
+const cssFileName = (entryName) => entryName.replace(`.js`, `.css`);
+
 const getEntries = () =>
   globby([`./docroot/themes/custom/**/src/*.js`, `./docroot/modules/custom/**/src/*.js`])
     .then((paths) => paths)
     .catch((error) => logError(error));
 
 module.exports = getEntries()
+  .then((allEntries) => {
+    console.clear();
+    return allEntries;
+  })
   .then((allEntries) =>
     allEntries.map((entry) => {
       const [outputPath, filename] = [`${entry.split(`src`)[0]}dist`, `${entry.split(`src/`)[1]}`];
@@ -26,60 +33,55 @@ module.exports = getEntries()
         MODE === `production`
           ? [
               new CleanWebpackPlugin(),
-              new MiniCssExtractPlugin({
-                moduleFilename: (_chunk) => filename.replace(`.js`, `.css`),
-              }),
+              new MiniCssExtractPlugin({ moduleFilename: (_chunk) => cssFileName(filename) }),
               new OptimizeCSSAssetsPlugin({}),
             ]
-          : [
-              new CleanWebpackPlugin(),
-              new MiniCssExtractPlugin({
-                moduleFilename: (_chunk) => filename.replace(`.js`, `.css`),
-              }),
-            ];
-      return {
-        mode: MODE,
-        entry,
-        output: {
-          path: path.join(__dirname, outputPath),
-          filename,
-        },
-        devtool: `hidden-source-map`,
-        externals: {
-          Drupal: `Drupal`,
-          drupalSettings: `drupalSettings`,
-        },
-        module: {
-          rules: [
-            {
-              test: /\.(js|jsx)/,
-              exclude: /node_modules/,
-              loader: ["babel-loader"],
-            },
-            {
-              test: /\.(css|scss)$/,
-              exclude: /node_modules/,
-              use: [
-                MiniCssExtractPlugin.loader,
-                { loader: "css-loader", options: { sourceMap: MODE === `development` } },
-                {
-                  loader: "postcss-loader",
-                  options: {
-                    ident: "postcss",
-                    plugins: [require("autoprefixer")],
+          : [new CleanWebpackPlugin(), new MiniCssExtractPlugin({ moduleFilename: (_chunk) => cssFileName(filename) })];
+      if (MODE !== `development` && MODE !== `production`)
+        return logError(
+          `This webpack configuration only supports 'development' or 'production' flags, please check the 'mode=' flag on the npm script you're running.`
+        );
+      else
+        return {
+          mode: MODE,
+          entry,
+          output: {
+            path: path.join(__dirname, outputPath),
+            filename,
+          },
+          module: {
+            rules: [
+              {
+                test: /\.(js|ts|jsx|tsx)/,
+                exclude: /node_modules/,
+                loader: ["babel-loader"],
+              },
+              {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  { loader: "css-loader", options: { sourceMap: MODE === `development` } },
+                  {
+                    loader: "postcss-loader",
+                    options: {
+                      ident: "postcss",
+                      plugins: [require("autoprefixer")],
+                    },
                   },
-                },
-                { loader: "sass-loader", options: { sourceMap: MODE === `development` } },
-              ],
-            },
-          ],
-        },
-        devtool: MODE === `development` ? `source-map` : false,
-        plugins: PLUGINS,
-        resolve: {
-          extensions: [".js", ".jsx", ".scss"],
-        },
-      };
+                  { loader: "sass-loader", options: { sourceMap: MODE === `development` } },
+                ],
+              },
+            ],
+          },
+          devtool: MODE === `development` ? `source-map` : false,
+          plugins: PLUGINS,
+          resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+            alias: resolveTsAliases(path.resolve("tsconfig.json")),
+          },
+          stats: `errors-only`,
+        };
     })
   )
   .catch((error) => logError(error));
